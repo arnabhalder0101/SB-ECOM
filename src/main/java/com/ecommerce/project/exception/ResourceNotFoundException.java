32bee598739e4267854d0eeefdbf966bde37293a
ARNAB HALDER <harnab308@gmail.com>
2026-09-01 00:31:12 +0530

    unused import removed 2

--- src/main/java/com/ecommerce/project/exception/ResourceNotFoundException.java
+++ src/main/java/com/ecommerce/project/exception/ResourceNotFoundException.java
@@ -2,7 +2,6 @@ package com.ecommerce.project.exception;
 
 public class ResourceNotFoundException extends RuntimeException{
 
-
     public ResourceNotFoundException(String resourceName,  String fieldName, String field) {
         super(String.format("%s is not found with %s: %s", resourceName, fieldName, field));
 
